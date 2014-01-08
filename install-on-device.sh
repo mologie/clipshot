d6b8b20922f77ae05da236536b8de294c4927a5d
Oliver Kuckertz <oliver.kuckertz@mologie.de>
2014-01-09 00:40:20 +0100

    Remove obsolete command

--- install-on-device.sh
+++ install-on-device.sh
@@ -6,6 +6,5 @@ if [ -z "$DEVICE" ]; then
 	exit 1
 fi
 make DEBUG=1
-#scp -r layout/Library/* root@$DEVICE:/Library
 scp ClipShot.plist .theos/obj/debug/ClipShot.dylib root@$DEVICE:/Library/MobileSubstrate/DynamicLibraries/
 ssh root@$DEVICE sbreload
